a35104871f367ff5714b40fa69b36e6c65bf4c87
brahma <brahma@gopassport.com>
2017-09-12 17:55:24 +0530

    add two pane layout

--- app/src/main/java/com/brahmachilakala/bakingapp/RecipeDetailActivity.java
+++ app/src/main/java/com/brahmachilakala/bakingapp/RecipeDetailActivity.java
@@ -2,6 +2,7 @@ package com.brahmachilakala.bakingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,27 +10,62 @@ import java.util.ArrayList;
 
 public class RecipeDetailActivity extends AppCompatActivity implements RecipeDetailFragment.IntentSendListener {
 
+    private boolean mTwoPane;
+    int position = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
 
-        Recipe recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+        if (findViewById(R.id.fragment_recipe_step) != null) {
+            mTwoPane = true;
 
-        RecipeDetailFragment fragment = RecipeDetailFragment.newInstance(recipe);
+            Recipe recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+            RecipeDetailFragment recipeDetailFragment = RecipeDetailFragment.newInstance(recipe);
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_recipe_detail, fragment)
-                .commit();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_recipe_detail, recipeDetailFragment)
+                    .commit();
+
+            ArrayList<Step> steps = recipe.getSteps();
+            RecipeStepFragment recipeStepFragment = RecipeStepFragment.newInstance(steps, position);
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_recipe_step, recipeStepFragment)
+                    .commit();
+
+
+
+        } else {
+            mTwoPane = false;
+
+            Recipe recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+            RecipeDetailFragment fragment = RecipeDetailFragment.newInstance(recipe);
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_recipe_detail, fragment)
+                    .commit();
+        }
 
     }
 
 
     @Override
     public void startIntent(ArrayList<Step> steps, int position) {
-        Intent recipeStepActivity = new Intent(RecipeDetailActivity.this, RecipeStepActivity.class);
-        recipeStepActivity.putExtra("steps", steps);
-        recipeStepActivity.putExtra("position", position);
-        startActivity(recipeStepActivity);
+
+        if (mTwoPane) {
+            RecipeStepFragment recipeStepFragment = RecipeStepFragment.newInstance(steps, position);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_recipe_step, recipeStepFragment)
+                    .commit();
+        } else {
+            Intent recipeStepActivity = new Intent(RecipeDetailActivity.this, RecipeStepActivity.class);
+            recipeStepActivity.putExtra("steps", steps);
+            recipeStepActivity.putExtra("position", position);
+            startActivity(recipeStepActivity);
+        }
     }
 }
